7fbe29d4e06f9cfe04f2856475f0c5bce7388fca
BuchynD <buchindaniil6@gmail.com>
2023-08-18 10:20:46 +0300

    Added native name to country card

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -5,10 +5,13 @@ interface CountryCardProps {
 }
 
 export default function CountryCard({ country }: CountryCardProps) {
+    const countryLanguage = Object.keys(country.name.nativeName)[0];
+
     return (
         <article className="bg-emerald-300 rounded p-2 border border-emerald-700 shadow-2xl">
             <img src={country.flags.png} alt={country.flags.alt} className="m-auto" />
             <h3 className="text-xl font-bold text-center">{country.name.common}</h3>
+            <strong className="text-lg font-bold text-center block">{country.name.nativeName[countryLanguage]?.common}</strong>
         </article>
     );
 }
